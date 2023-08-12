ff0a61547bbf17580203a9e3291b8aee5116f895
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-08-12 20:07:20 +0900

    Mod:Default Typography

--- presentation/src/main/java/com/kusitms/presentation/common/theme/Type.kt
+++ presentation/src/main/java/com/kusitms/presentation/common/theme/Type.kt
@@ -21,12 +21,14 @@ data class CustomTypography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.SemiBold,
         fontSize =  28.sp,
+        lineHeight = 36.sp,
         letterSpacing = (-1.5).sp
     ),
     val Header2: TextStyle = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.SemiBold,
         fontSize =  24.sp,
+        lineHeight = 32.sp,
         letterSpacing = (-1.5).sp
     ),
 
@@ -34,12 +36,14 @@ data class CustomTypography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
         fontSize =  20.sp,
+        lineHeight = 28.sp,
         letterSpacing = (-1.5).sp
     ),
     val SubTitle1_Semibold: TextStyle = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.SemiBold,
         fontSize =  20.sp,
+        lineHeight = 28.sp,
         letterSpacing = (-1.5).sp
     ),
 
@@ -47,12 +51,14 @@ data class CustomTypography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
         fontSize =  18.sp,
+        lineHeight = 24.sp,
         letterSpacing = (-1.5).sp
     ),
     val SubTitle2_Semibold: TextStyle = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.SemiBold,
         fontSize =  18.sp,
+        lineHeight = 24.sp,
         letterSpacing = (-1.5).sp
     ),
 
@@ -60,12 +66,14 @@ data class CustomTypography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
         fontSize =  16.sp,
+        lineHeight = 24.sp,
         letterSpacing = (-1.5).sp
     ),
     val Text_Semibold: TextStyle = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.SemiBold,
         fontSize =  16.sp,
+        lineHeight = 24.sp,
         letterSpacing = (-1.5).sp
     ),
 
@@ -73,12 +81,14 @@ data class CustomTypography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
         fontSize =  14.sp,
+        lineHeight = 20.sp,
         letterSpacing = (-1.5).sp
     ),
     val Caption2: TextStyle = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
         fontSize =  12.sp,
+        lineHeight = 18.sp,
         letterSpacing = (-1.5).sp
     ),
 
@@ -86,12 +96,14 @@ data class CustomTypography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
         fontSize =  16.sp,
+        lineHeight = 24.sp,
         letterSpacing = (-1.5).sp
     ),
     val Body2: TextStyle = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
         fontSize =  14.sp,
+        lineHeight = 22.sp,
         letterSpacing = (-1.5).sp
     ),
 
