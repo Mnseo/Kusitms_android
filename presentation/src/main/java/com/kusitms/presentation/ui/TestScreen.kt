b4407ff297d79720e3ed0da2700c9bd3ef6567d7
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-08-17 16:24:50 +0900

    Mod: Modifier Gradient

--- presentation/src/main/java/com/kusitms/presentation/ui/TestScreen.kt
+++ presentation/src/main/java/com/kusitms/presentation/ui/TestScreen.kt
@@ -2,8 +2,16 @@ package com.kusitms.presentation.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,19 +24,24 @@ fun TestScreen() {
 
 }
 
-@Preview(showBackground = true)
+
+@Composable
+fun Color(color: Long): Color {
+    return Color(value = (color.toULong()and 0xffffffffUL) shl 32)
+}
+
+@Preview(showBackground = true, widthDp =  200, heightDp = 200)
 @Composable
 fun Test() {
-    Box(
-        Modifier
-            .width(400.dp)
-            .background(color = KusitmsColorPalette.current.Main700)
-            .height(400.dp)) {
-
-        Box(
+    Box(modifier = Modifier.padding(12.dp)) {
+        Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(color = KusitmsColorPalette.current.Main500)
-        )
+                .background(
+                    brush = Brush.verticalGradient(listOf(Color.Red, Color.Blue)),
+                    shape = CircleShape,
+                    alpha = 0.4f
+                )
+        ) {}
     }
 }
