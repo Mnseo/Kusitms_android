6866d673201b846cc10eaaaf070d2e4dc563e00b
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-09-01 15:25:10 +0900

    Mod: ImageCard

--- presentation/src/main/java/com/kusitms/presentation/ui/login/LoginNonMemberScreen.kt
+++ presentation/src/main/java/com/kusitms/presentation/ui/login/LoginNonMemberScreen.kt
@@ -1,20 +1,71 @@
 package com.kusitms.presentation.ui.login
 
+import android.service.autofill.CustomDescription
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.kusitms.presentation.common.ui.theme.KusitmsColorPalette
+import com.kusitms.presentation.common.ui.theme.KustimsTypo
 
 
 @Composable
 fun LoginNonMember(navController: NavController) {
-
+    val painter = painterResource(id = drawable)
+    ImageCard(painter = , contentDescription = , title = )
 
 }
 
 
 
-@Composable
-fun officialSite() {
 
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(16.dp),
+
+    ) {
+        Box(modifier =
+        Modifier
+            .width(154.24.dp)
+            .height(154.24.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(color = KusitmsColorPalette.current.Main500)
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            contentAlignment = Alignment.BottomStart){
+                Text(title,style= KustimsTypo.current.Body1, color = KusitmsColorPalette.current.Main200)
+            }
+        }
+
+    }
 }
 
 @Composable
