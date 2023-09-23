f3bdda254a269ff6a8cf6045ce0e5781590b48e9
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-09-23 12:31:12 +0900

    Add: SignInCondition

--- domain/src/main/java/com/kusitms/domain/usecase/LoginUseCase.kt
+++ domain/src/main/java/com/kusitms/domain/usecase/LoginUseCase.kt
@@ -1,8 +1,5 @@
 package com.kusitms.domain.usecase
 
-import com.kusitms.domain.repository.LoginRepository
-import javax.inject.Inject
-
 class LoginUseCase {
 
 }
--- presentation/src/main/java/com/kusitms/presentation/ui/login/LoginViewModel.kt
+++ presentation/src/main/java/com/kusitms/presentation/ui/login/LoginViewModel.kt
@@ -1,7 +1,2 @@
 package com.kusitms.presentation.ui.login
 
-import androidx.lifecycle.ViewModel
-import com.kusitms.domain.usecase.LoginUseCase
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
