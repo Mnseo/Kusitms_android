a80e6dc152269598a50ec8653fa9ebd925572ce4
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-09-23 11:43:56 +0900

    Add: Screen3

--- presentation/src/main/java/com/kusitms/presentation/ui/signIn/SignInScreen3.kt
+++ presentation/src/main/java/com/kusitms/presentation/ui/signIn/SignInScreen3.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.ImageLoader
@@ -15,6 +16,7 @@ import coil.compose.rememberAsyncImagePainter
 import coil.decode.SvgDecoder
 import com.kusitms.presentation.R
 import com.kusitms.presentation.common.ui.theme.KusitmsColorPalette
+import com.kusitms.presentation.common.ui.theme.KusitmsTypo
 
 @Composable
 fun SignInScreen3(navController: NavController) {
@@ -28,7 +30,8 @@ fun SignInScreen3(navController: NavController) {
         Spacer(modifier = Modifier.height(122.5.dp))
         rocket()
         
-        Text(text = )
+        Text(text = stringResource(id = R.string.signin3_text1), style= KusitmsTypo.current.Text_Semibold, color = KusitmsColorPalette.current.Grey300)
+
         
     }
 }
@@ -49,3 +52,10 @@ fun rocket() {
             .width(200.dp)
     )
 }
+
+@Composable
+fun nameText(name: String) {
+    Text(text = name, style = KusitmsTypo.current.Header1, color = KusitmsColorPalette.current.White)
+    Text(text = stringResource(id = R.string.signin3_text2), style = KusitmsTypo.current.Text_Medium, color = KusitmsColorPalette.current.Grey400)
+}
+
