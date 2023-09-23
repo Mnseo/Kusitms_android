32064be17a460103c360e6d851cbbce2b95930c4
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-09-23 12:38:28 +0900

    Add: linkCategory model

---
+++ domain/src/main/java/com/kusitms/domain/model/SignInExtraCondition.kt
@@ -0,0 +1,8 @@
+package com.kusitms.domain.model
+
+data class SignInExtraCondition(
+    val imageUrl: String? = null,
+    val introduce: String? = null,
+    val linkCategory: String? = null,
+    val
+)
---
+++ domain/src/main/java/com/kusitms/domain/model/linkCategory.kt
@@ -0,0 +1,11 @@
+package com.kusitms.domain.model
+
+enum class linkCategory {
+    LINK,
+    BRUNCH,
+    TISTORY,
+    NOTION,
+    BEHANCE,
+    LINKEDIN,
+    GITHUB
+}
