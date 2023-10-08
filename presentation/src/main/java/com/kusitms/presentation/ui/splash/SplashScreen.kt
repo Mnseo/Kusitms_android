4db16f1009320f24c2b21627b07ae0dcd8ebef8d
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-10-08 16:23:26 +0900

    Chore: Splash compose - Screen 분리

--- presentation/src/main/java/com/kusitms/presentation/ui/splash/SplashScreen.kt
+++ presentation/src/main/java/com/kusitms/presentation/ui/splash/SplashScreen.kt
@@ -1,83 +1,51 @@
 package com.kusitms.presentation.ui.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
+import com.kusitms.presentation.R
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.google.android.material.color.utilities.MaterialDynamicColors.background
 import com.kusitms.presentation.common.ui.theme.KusitmsColorPalette
-import com.kusitms.presentation.common.ui.theme.KusitmsTheme
-import com.kusitms.presentation.common.ui.theme.Pretendard
-import com.kusitms.presentation.navigation.NavRoutes
+import com.kusitms.presentation.common.ui.theme.KusitmsTypo
 
-@Composable
-fun Splash(navController: NavHostController) {
-    KusitmsTheme(darkTheme = true) {
-        // NavHostController TestCode
-        Surface(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(24.dp)
-                .background(color = KusitmsColorPalette.current.Grey900)
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .wrapContentSize(Alignment.Center)
-                    .background(color = KusitmsColorPalette.current.Grey100)
-
-            ) {
-                Text(
-                    text = "비전을 가지고 함께 성장하는 학회", textAlign = TextAlign.Center, fontSize = 20.sp, fontFamily = Pretendard, fontWeight = FontWeight.Medium,
-                )
-                Spacer(modifier = Modifier.height(20.dp))
-                Row(
-                    modifier = Modifier.wrapContentWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    TestNavButton(route = NavRoutes.Login.route, navController = navController)
-                    TestNavButton(route = NavRoutes.SignIn.route, navController = navController)
-                    TestNavButton(route = NavRoutes.Home.route, navController = navController)
-                    TestNavButton(route = NavRoutes.Setting.route, navController = navController)
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun TestNavButton(route: String, navController: NavHostController, modifier: Modifier = Modifier) {
-    Button(
-        onClick = {
-            navController.navigate(route) {
-                popUpTo(route) {
-                    inclusive = true
-                }
-            }
-        },
-        modifier = modifier.wrapContentSize(),
-    ) {
-        Text(
-            text = "Go to ${route}"
-        )
-    }
-}
 
 @Preview
 @Composable
-fun SplashScreen() {
-    Splash(navController = rememberNavController())
+fun Splash() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
+        splashLogo()
+        Spacer(modifier = Modifier.height(50.dp))
+        Text(text = stringResource(id = R.string.splash_tv), style = KusitmsTypo.current.Caption1, color = KusitmsColorPalette.current.White)
+    }
+
+    
 }
+
+@Composable
+fun splashLogo() {
+    Row (
+        modifier = Modifier
+            .width(225.dp)
+            .background(color = KusitmsColorPalette.current.Grey900)
+            .height(63.dp)
+            .padding(0.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.Start)
+        ) {
+        Image(painter = painterResource(id = R.drawable.ic_splash_kusitms), contentDescription = null)
+        Image(painter = painterResource(id = R.drawable.ic_splash_plus), contentDescription = null)
+    }
+}
+
