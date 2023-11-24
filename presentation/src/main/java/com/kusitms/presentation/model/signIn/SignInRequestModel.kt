3cbdbf0e95c6bba4c0e901028606ec94141b23b3
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-11-24 17:10:56 +0900

    Add: SignInRequestModel 로직

--- presentation/src/main/java/com/kusitms/presentation/model/signIn/SignInRequestModel.kt
+++ presentation/src/main/java/com/kusitms/presentation/model/signIn/SignInRequestModel.kt
@@ -2,8 +2,31 @@ package com.kusitms.presentation.model.signIn
 
 import androidx.compose.runtime.MutableState
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class SignInRequestModel: ViewModel() {
-    private val email = MutableStateFlow("kusitms1234@naver.com")
+    private val _email = MutableStateFlow("kusitms1234@naver.com")
+    val email: StateFlow<String> = _email
+
+    private val _emailInputState = MutableStateFlow(EmailInputState.DEFAULT)
+    val emailInputState: StateFlow<EmailInputState> = _emailInputState
+
+    val isEmailValid: Boolean
+        get() = email.value == "kusitms1234@naver.com"
+
+    fun updateEmail(newEmail: String) {
+        _email.value = newEmail
+        _emailInputState.value = if (newEmail.isBlank()) EmailInputState.DEFAULT else EmailInputState.ENTERED
+    }
+
+    fun validateEmail() {
+        _emailInputState.value = if(isEmailValid) EmailInputState.VALID else EmailInputState.INVALID
+    }
+
+    enum class EmailInputState {
+        DEFAULT, ENTERED, VALID, INVALID
+    }
+
 }
--- presentation/src/main/java/com/kusitms/presentation/ui/signIn/SignInRequestScreen.kt
+++ presentation/src/main/java/com/kusitms/presentation/ui/signIn/SignInRequestScreen.kt
@@ -1,13 +1,13 @@
 package com.kusitms.presentation.ui.signIn
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.kusitms.presentation.R
 import com.kusitms.presentation.common.theme.KusitmsScaffoldNonScroll
@@ -15,17 +15,19 @@ import com.kusitms.presentation.common.ui.KusitmsMarginVerticalSpacer
 import com.kusitms.presentation.common.ui.theme.KusitmsColorPalette
 import com.kusitms.presentation.common.ui.theme.KusitmsTypo
 import com.kusitms.presentation.model.signIn.SignInRequestModel
+import com.kusitms.presentation.ui.login.manager.LoginManagerInputColumn
+import com.kusitms.presentation.ui.login.member.LoginMemberPwInput
 
 
 @Composable
-fun SignInRequestScreen(navController: NavHostController) {
+fun SignInRequestScreen(viewModel: SignInRequestModel, navController: NavHostController) {
     KusitmsScaffoldNonScroll(topbarText = stringResource(id = R.string.signin_request_topbar), navController = navController) {
-        
+        SignInRequestColumn(viewModel = viewModel)
     }
 }
 
 @Composable
-fun SignInRequestColumn() {
+fun SignInRequestColumn(viewModel: SignInRequestModel) {
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -33,20 +35,61 @@ fun SignInRequestColumn() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         KusitmsMarginVerticalSpacer(size = 120)
-        Text(text = stringResource(id = R.string.signin_request_title), style= KusitmsTypo.current.SubTitle2_Semibold, color = KusitmsColorPalette.current.Grey300)
+        SignInRequestSubColumn1(viewModel = viewModel)
 
     }
 
 }
 
 @Composable
-fun SignInRequestSubColumn() {
+fun SignInRequestSubColumn1(viewModel: SignInRequestModel) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .height(450.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(text = stringResource(id = R.string.signin_request_title), style= KusitmsTypo.current.SubTitle2_Semibold, color = KusitmsColorPalette.current.Grey300)
+        Spacer(modifier = Modifier.height(72.dp))
+        SignInRequestSubColumn2(viewModel = viewModel)
+    }
+}
 
+@Composable
+fun SignInRequestSubColumn2(viewModel: SignInRequestModel) {
+    val email = viewModel.email.collectAsState()
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .height(200.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        androidx.compose.material3.Text(
+            text= stringResource(id = R.string.signin_request_caption1),
+            style = KusitmsTypo.current.Caption1,
+            color = KusitmsColorPalette.current.Grey400
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        KusitmsInputField(text = R.string.signin_request_placeholder1, value = email.value, onValueChange = {viewModel.updateEmail(it)})
+        Spacer(modifier = Modifier.height(24.dp))
+
+        androidx.compose.material3.Text(
+            text= stringResource(id = R.string.signin_request_caption2),
+            style = KusitmsTypo.current.Caption1,
+            color = KusitmsColorPalette.current.Grey400
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        KusitmsInputField(text = R.string.signin_request_placeholder2)
+        Spacer(modifier = Modifier.height(24.dp))
 }
 
 
 @Composable
-fun SigInRequestBtn() {
+fun SigInRequestBtn(viewModel: SignInRequestModel) {
+    val buttonColor = if(viewModel.isEmailValid) {
+
+    }
+}
 
 }
 
