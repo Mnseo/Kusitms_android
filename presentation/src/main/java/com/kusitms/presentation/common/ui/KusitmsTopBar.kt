036499b456a1484ecb30e06d450efa310ed2aa45
Jung SeokJoon <eshc.joon@gmail.com>
2023-12-09 14:12:24 +0900

    MOD : TopBarTextWithTwoIcons

--- presentation/src/main/java/com/kusitms/presentation/common/ui/KusitmsTopBar.kt
+++ presentation/src/main/java/com/kusitms/presentation/common/ui/KusitmsTopBar.kt
@@ -1,10 +1,13 @@
 package com.kusitms.presentation.common.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.InlineTextContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -33,6 +36,40 @@ fun KusitsmTopBarTextWithIcon(
         Spacer(modifier = Modifier.weight(1f))
 
         iconContent()
+
+    }
+}
+
+@Composable
+fun KusitsmTopBarTextWithTwoIcons(
+    textContent: @Composable () -> Unit,
+    firstIconContent : @Composable () -> Unit,
+    secondIconContent : @Composable () -> Unit,
+){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .padding(horizontal = 12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Box(
+            modifier = Modifier.size(32.dp),
+            contentAlignment = Alignment.Center
+        ){
+            firstIconContent()
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        textContent()
+
+        Spacer(modifier = Modifier.weight(1f))
+        Box(
+            modifier = Modifier.size(32.dp),
+            contentAlignment = Alignment.Center
+        ){
+            secondIconContent()
+        }
+
 
     }
 }
