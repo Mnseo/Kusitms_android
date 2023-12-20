082e656ab24fae01ab15f9f81d09e9dbe7ee3d94
arinming <jyandysys1@naver.com>
2023-12-20 23:00:59 +0900

    Add: 프로필 상단 헤더, 파트 선택 카드 추가

---
+++ presentation/src/main/java/com/kusitms/presentation/ui/profile/ProfileScreen.kt
@@ -0,0 +1,95 @@
+package com.kusitms.presentation.ui.profile
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kusitms.presentation.common.ui.KusitmsMarginHorizontalSpacer
+import com.kusitms.presentation.common.ui.KusitmsMarginVerticalSpacer
+import com.kusitms.presentation.common.ui.KusitsmTopBarTextWithIcon
+import com.kusitms.presentation.common.ui.theme.KusitmsColorPalette
+import com.kusitms.presentation.common.ui.theme.KusitmsTypo
+import com.kusitms.presentation.ui.ImageVector.icons.KusitmsIcons
+import com.kusitms.presentation.ui.ImageVector.icons.kusitmsicons.ArrowDown
+import com.kusitms.presentation.ui.ImageVector.icons.kusitmsicons.Search
+import com.kusitms.presentation.ui.notice.CurriculumNoticeDropdown
+
+@Composable
+fun ProfileScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(KusitmsColorPalette.current.Grey900),
+    ) {
+        KusitsmTopBarTextWithIcon(text = "프로필", iconContent = {
+            Image(
+                modifier = Modifier
+                    .size(24.dp)
+                    .clickable { },
+                imageVector = KusitmsIcons.Search,
+                contentDescription = "검색"
+            )
+        })
+        KusitmsMarginVerticalSpacer(size = 32)
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .clickable { },
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = KusitmsColorPalette.current.Grey700,
+                contentColor = KusitmsColorPalette.current.Grey100
+            )
+        ) {
+            Row(
+                modifier = Modifier
+                    .height(56.dp)
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = "파트 선택",
+                    style = KusitmsTypo.current.Text_Medium,
+                    color = KusitmsColorPalette.current.Grey100,
+                    modifier = Modifier.weight(1f)
+                )
+                Icon(
+                    modifier = Modifier
+                        .size(24.dp)
+                        .clickable {
+                        },
+                    imageVector = KusitmsIcons.ArrowDown,
+                    contentDescription = null,
+                    tint = KusitmsColorPalette.current.Grey400,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen()
+}
