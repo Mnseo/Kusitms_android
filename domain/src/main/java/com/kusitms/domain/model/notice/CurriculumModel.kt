595f6972236f6b7faac9f0f4ee80b361831ba9d2
Jung SeokJoon <eshc.joon@gmail.com>
2023-12-21 17:53:29 +0900

    CHORE : 더미데이터 삭제

--- domain/src/main/java/com/kusitms/domain/model/notice/CurriculumModel.kt
+++ domain/src/main/java/com/kusitms/domain/model/notice/CurriculumModel.kt
@@ -6,20 +6,3 @@ data class CurriculumModel(
     val title : String = "",
     val curriculumNoticeList : List<NoticeModel> = emptyList()
 )
-
-val curriculumDummy = listOf(
-    CurriculumModel(
-        1,
-        title = "전체OT",
-    ),
-    CurriculumModel(
-        2,
-        title = "기업프로젝트",
-        curriculumNoticeList = emptyList()
-    ),
-    CurriculumModel(
-        3,
-        title = "큐커톤",
-        curriculumNoticeList = noticeDummy.take(2)
-    )
-)
