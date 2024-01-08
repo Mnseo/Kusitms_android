8aee24bb963f02f69ee0e8349d01c599e3d6fe00
arinming <jyandysys1@naver.com>
2024-01-08 23:48:21 +0900

    ADD: 프로필 리스트 받아오기 유스케이스 추가 (#74)

--- presentation/src/main/java/com/kusitms/presentation/model/profile/ProfileViewModel.kt
+++ presentation/src/main/java/com/kusitms/presentation/model/profile/ProfileViewModel.kt
@@ -2,14 +2,30 @@ package com.kusitms.presentation.model.profile
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kusitms.domain.usecase.profile.GetProfileListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor() : ViewModel() {
+class ProfileViewModel @Inject constructor(
+    getProfileListUseCase: GetProfileListUseCase,
+) : ViewModel() {
+
+    val profileList = getProfileListUseCase().catch {
+
+    }.stateIn(
+        viewModelScope,
+        started = SharingStarted.Eagerly,
+        initialValue = emptyList()
+    )
+
     private val _uiState = MutableStateFlow(ProfileUiState())
     val uiState: StateFlow<ProfileUiState> = _uiState.asStateFlow()
 
