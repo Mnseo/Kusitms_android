e21a3c82238656533dd009f40d790c005258d199
arinming <jyandysys1@naver.com>
2024-01-10 21:58:02 +0900

    FIX: API 경로에 v1 추가 (#74)

--- data/src/main/java/com/kusitms/data/remote/api/KusitmsApi.kt
+++ data/src/main/java/com/kusitms/data/remote/api/KusitmsApi.kt
@@ -23,71 +23,71 @@ import retrofit2.http.*
 
 
 interface KusitmsApi {
-    @GET("auth/login/MEMBER")
+    @GET("v1/auth/login/MEMBER")
     suspend fun loginMember(
         @Query("email") email: String,
         @Query("password") password: String,
     ): LoginResponse
 
     @Multipart
-    @POST("member")
+    @POST("v1/member")
     suspend fun sendAdditionalProfile(
         @Part("dto") dto: RequestBody,
         @Part file: MultipartBody.Part,
     ): Response<BaseResponse<Unit?>?>
 
 
-    @GET("member/info")
+    @GET("v1/member/info")
     suspend fun loginMemberProfile(): LoginMemberProfileResponse
 
     // 공지사항 -> 차후에 분리하는 것도 좋을 듯 싶습니다.
-    @GET("notice")
+    @GET("v1/notice")
     suspend fun getNoticeList(): BaseResponse<List<NoticePayload>>
 
-    @GET("notice/{noticeId}/detail")
+    @GET("v1/notice/{noticeId}/detail")
     suspend fun getNoticeDetail(
         @Path("noticeId") noticeId: Int,
     ): BaseResponse<NoticePayload>
 
-    @GET("notice/curriculum")
+    @GET("v1/notice/curriculum")
     suspend fun getCurriculumList(): BaseResponse<List<CurriculumPayload>>
 
-    @GET("notice/curriculum/list")
+    @GET("v1/notice/curriculum/list")
     suspend fun getGetCurriculumNoticeList(
         @Query("curriculumId") curriculumId: Int,
     ): BaseResponse<List<NoticePayload>>
 
-    @GET("comment/{noticeId}")
+    @GET("v1/comment/{noticeId}")
     suspend fun getNoticeCommentList(
         @Path("noticeId") noticeId: Int,
     ): BaseResponse<List<CommentPayload>>
 
-    @POST("comment/{noticeId}")
+    @POST("v1/comment/{noticeId}")
     suspend fun addNoticeComment(
         @Path("noticeId") noticeId: Int,
         @Body commentContentRequestBody: CommentContentRequestBody,
     ): BaseResponse<CommentPayload>
 
-    @DELETE("comment/{commentId}")
+    @DELETE("v1/comment/{commentId}")
     suspend fun deleteNoticeComment(
         @Path("commentId") commentId: Int,
     ): BaseResponse<Unit>
 
-    @POST("report")
+    @POST("v1/report")
     suspend fun reportComment(
         @Body reportCommentRequestBody: ReportCommentRequestBody,
     ): Response<BaseResponse<Unit>>
 
     // SignInNonMember
     @FormUrlEncoded
-    @POST("member/check/register")
+    @POST("v1/member/check/register")
     suspend fun signInRequestCheck(
         @Field("email") email: String,
         @Field("password") password: String,
     ): SignInRequestResponse
 
     @FormUrlEncoded
-    @POST("member/register")
+    @POST("v1/member/register")
     suspend fun signInRequest(
         @Field("email") email: String,
         @Field("password") password: String,
@@ -95,51 +95,51 @@ interface KusitmsApi {
 
     // FindPw
     @FormUrlEncoded
-    @POST("member/email")
+    @POST("v1/member/email")
     suspend fun verifyEmailCheck(
         @Field("email") email: String,
     ): FindPwCheckEmailResponse
 
     @FormUrlEncoded
-    @POST("member/verify")
+    @POST("v1/member/verify")
     suspend fun sendCode(
         @Field("email") email: String,
     ): BaseResponse<Unit>
 
     @FormUrlEncoded
-    @POST("member/verify/code")
+    @POST("v1/member/verify/code")
     suspend fun verifyCode(
         @Field("email") email: String,
         @Field("code") code: String,
     ): FindPwCodeVerifyResponse
 
-    @PUT("member/password/unauthenticated")
+    @PUT("v1/member/password/unauthenticated")
     suspend fun updatePassword(
         @Query("email") email: String,
         @Body passwordRequest: UpdatePasswordRequest,
     ): BaseResponse<Unit>
 
 
-    @POST("member/password")
+    @POST("v1/member/password")
     suspend fun checkPassword(
         @Query("password") password: String,
     ): BaseResponse<CheckPasswordPayload>
 
-    @PUT("member/password")
+    @PUT("v1/member/password")
     suspend fun updatePasswordAsLoggedIn(
         @Body passwordRequest: UpdatePasswordRequest,
     ): BaseResponse<Unit>
 
     //로그아웃 및 탈퇴
-    @DELETE("auth/logout")
+    @DELETE("v1/auth/logout")
     suspend fun logOutMember(): BaseResponse<Unit>
 
-    @DELETE("member")
+    @DELETE("v1/member")
     suspend fun signOutMember(): BaseResponse<Unit>
 
 
     // 프로필 리스트
-    @GET("member/info/list")
+    @GET("v1/member/info/list")
     suspend fun getInfoListMember(): BaseResponse<List<ProfilePayload>>
 
 }
