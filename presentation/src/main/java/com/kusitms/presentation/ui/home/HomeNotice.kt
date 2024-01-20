1bdfda00e83954639b75996dd59ca3791a5a5941
arinming <jyandysys1@naver.com>
2024-01-21 03:09:11 +0900

    FEAT: 공지 애니메이션 구현 (#85)

--- presentation/src/main/java/com/kusitms/presentation/ui/home/HomeNotice.kt
+++ presentation/src/main/java/com/kusitms/presentation/ui/home/HomeNotice.kt
@@ -1,6 +1,14 @@
 package com.kusitms.presentation.ui.home
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,11 +20,11 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush.Companion.verticalGradient
+import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,7 +33,6 @@ import com.kusitms.domain.model.home.NoticeRecentModel
 import com.kusitms.presentation.R
 import com.kusitms.presentation.common.ui.theme.KusitmsColorPalette
 import com.kusitms.presentation.common.ui.theme.KusitmsTypo
-import kotlinx.coroutines.flow.MutableStateFlow
 
 @Composable
 fun HomeNotice(
@@ -44,7 +51,7 @@ fun HomeNotice(
     }
 }
 
-
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun HomeNoticeExist(
     notice: List<NoticeRecentModel>,
@@ -70,20 +77,68 @@ fun HomeNoticeExist(
                 contentDescription = stringResource(id = R.string.home_ic_notice),
             )
             Spacer(modifier = Modifier.width(8.dp))
+
             Column {
-                Text(
-                    text = notice[currentNoticeIndex.value].title,
-                    style = KusitmsTypo.current.Text_Semibold,
-                    color = KusitmsColorPalette.current.White
-                )
-                Text(
-                    text = notice[nextNoticeIndex.value].title,
-                    style = KusitmsTypo.current.Text_Semibold,
-                    color = KusitmsColorPalette.current.White,
-                    modifier = Modifier
-                        .padding(top = 4.dp)
-                )
+                AnimatedContent(
+                    targetState = notice[currentNoticeIndex.value].title,
+                    transitionSpec = {
+                        slideIntoContainer(
+                            towards = AnimatedContentTransitionScope.SlideDirection.Up,
+                            animationSpec = tween(durationMillis = 500)
+                        ) togetherWith
+                                slideOutOfContainer(
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Up,
+                                    animationSpec = tween(durationMillis = 500)
+                                )
+                    },
+                    contentAlignment = Alignment.Center
+                ) { targetCount ->
+                    Column {
+                        Text(
+                            text = targetCount,
+                            style = KusitmsTypo.current.Text_Semibold,
+                            color = KusitmsColorPalette.current.White,
+                            modifier = Modifier.clickable { }
+                        )
+                    }
+                }
+
+                AnimatedContent(
+                    targetState = notice[nextNoticeIndex.value].title,
+                    transitionSpec = {
+                        slideIntoContainer(
+                            towards = AnimatedContentTransitionScope.SlideDirection.Up,
+                            animationSpec = tween(durationMillis = 500)
+                        ) togetherWith
+                                slideOutOfContainer(
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Up,
+                                    animationSpec = tween(durationMillis = 500)
+                                )
+                    },
+                    contentAlignment = Alignment.Center
+                ) { targetCount ->
+
+                    Text(
+                        text = targetCount,
+                        style = KusitmsTypo.current.Text_Semibold,
+                        color = KusitmsColorPalette.current.White,
+                    )
+                    Box(
+                        modifier = Modifier
+                            .padding(top = 4.dp)
+                            .fillMaxWidth()
+                            .height(16.dp)
+                            .background(
+                                brush = verticalGradient(
+                                    0f to KusitmsColorPalette.current.Grey900.copy(alpha = 0f),
+                                    0.7f to KusitmsColorPalette.current.Grey900,
+                                    tileMode = TileMode.Clamp
+                                )
+                            )
+                    )
+                }
             }
+
         }
     }
 }
