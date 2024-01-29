65a681dab0a45a6f3e5679165bfc97b37030fbde
Jung SeokJoon <eshc.joon@gmail.com>
2024-01-29 21:59:31 +0900

    mod NoticePayload 수정

--- data/src/main/java/com/kusitms/data/remote/entity/response/notice/NoticePayload.kt
+++ data/src/main/java/com/kusitms/data/remote/entity/response/notice/NoticePayload.kt
@@ -4,6 +4,19 @@ import com.kusitms.domain.model.notice.NoticeModel
 
 data class NoticePayload(
     val noticeId : Int? = 0,
+    val curriculumName : String? = "",
+    val teamName : String? = "",
+    val title : String? = "",
+    val content : String? = "",
+    val date : String? = "",
+    val imageUrl : List<String>? = emptyList(),
+    val viewYn : Boolean? = false,
+    val name : String? = "",
+    val profileImage : String? = ""
+)
+
+data class NoticeDetailPayload(
+    val noticeId : Int? = 0,
     val curriculum : String? = "",
     val team : String? = "",
     val title : String? = "",
@@ -18,8 +31,22 @@ data class NoticePayload(
 fun NoticePayload.toModel() =
     NoticeModel(
         noticeId = noticeId ?: 0,
-        curriculum = curriculum ?: "",
-        team = team ?: "",
+        curriculumName = curriculumName ?: "",
+        teamName = teamName ?: "",
+        title = title ?: "",
+        content = content ?: "",
+        date = date ?: "",
+        imageUrl = imageUrl ?: emptyList(),
+        viewYn = viewYn ?: false,
+        name = name ?: "",
+        profileImage = profileImage ?: ""
+    )
+
+fun NoticeDetailPayload.toModel() =
+    NoticeModel(
+        noticeId = noticeId ?: 0,
+        curriculumName = curriculum ?: "",
+        teamName = team ?: "",
         title = title ?: "",
         content = content ?: "",
         date = date ?: "",
