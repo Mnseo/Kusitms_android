47131aff9acb4b9c204651a5b321a1d1c51a4e1c
arinming <jyandysys1@naver.com>
2024-02-07 19:38:45 +0900

    FEAT: 이름, 전공 입력 (#101)

--- presentation/src/main/java/com/kusitms/presentation/model/profile/edit/ProfileEditViewModel.kt
+++ presentation/src/main/java/com/kusitms/presentation/model/profile/edit/ProfileEditViewModel.kt
@@ -1,6 +1,11 @@
 package com.kusitms.presentation.model.profile.edit
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
+import com.kusitms.presentation.model.signIn.InterestItem
+import com.kusitms.presentation.model.signIn.LinkItem
+import com.kusitms.presentation.model.signIn.LinkType
+import com.kusitms.presentation.model.signIn.SignInStatus
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,8 +19,44 @@ class ProfileEditViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(ProfileEditUiState())
     val uiState: StateFlow<ProfileEditUiState> = _uiState.asStateFlow()
 
+    private val _isAllFieldsValid = MutableStateFlow(false)
+    val isAllFieldsValid: StateFlow<Boolean> = _isAllFieldsValid
+
     private val _expanded = MutableStateFlow(false)
     val expended: StateFlow<Boolean> = _expanded.asStateFlow()
+
+    private val _favoriteCategory = MutableStateFlow<List<String>?>(null)
+    val favoriteCategory: StateFlow<List<String>?> = _favoriteCategory
+
+    private val _interests = MutableStateFlow<List<InterestItem>>(emptyList())
+    val interests: StateFlow<List<InterestItem>> = _interests.asStateFlow()
+
+    private val _name = MutableStateFlow("")
+    val name: StateFlow<String> = _name
+
+    private val _major = MutableStateFlow("")
+    val major: StateFlow<String> = _major
+
+    private val _phoneNum = MutableStateFlow("")
+    val phoneNum: StateFlow<String> = _phoneNum
+
+    private val _email = MutableStateFlow("")
+    val email: StateFlow<String> = _email
+
+    private val _selectedImage = MutableStateFlow<Uri?>(null)
+    val selectedImage: StateFlow<Uri?> = _selectedImage.asStateFlow()
+
+    private val _linkItems = MutableStateFlow<List<LinkItem>>(listOf(LinkItem(LinkType.LINK, "")))
+    val linkItems: StateFlow<List<LinkItem>> = _linkItems.asStateFlow()
+
+    private val _introduce = MutableStateFlow("")
+    val introduce: StateFlow<String> = _introduce
+
+    private val _signInStatus = MutableStateFlow(SignInStatus.DEFAULT)
+    val signInStatus : StateFlow<SignInStatus> = _signInStatus
+
+    private val _selectedPart = MutableStateFlow<String?>(null)
+    val selectedPart: StateFlow<String?> = _selectedPart
 
     fun changeSelectProfileFilter(category: String) {
         _uiState.value = _uiState.value.copy(currentSelectedProfileFilter = category)
@@ -24,5 +65,16 @@ class ProfileEditViewModel @Inject constructor(
 
     fun toggleExpanded() {
         _expanded.value = !_expanded.value
+    }
+
+    fun updateMajor(newMajor: String) {
+        _major.value = newMajor
+        validateFields()
+    }
+
+    private fun validateFields() {
+        _isAllFieldsValid.value = _major.value.isNotBlank() &&
+                _selectedPart.value != null &&
+                _interests.value.isNotEmpty()
     }
 }
--- presentation/src/main/java/com/kusitms/presentation/ui/profile/edit/ProfileEditScreen.kt
+++ presentation/src/main/java/com/kusitms/presentation/ui/profile/edit/ProfileEditScreen.kt
@@ -6,11 +6,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -19,6 +19,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -35,8 +38,12 @@ import com.kusitms.presentation.common.ui.theme.KusitmsTypo
 import com.kusitms.presentation.model.profile.edit.ProfileEditViewModel
 import com.kusitms.presentation.model.profile.edit.ProfileFilterList
 import com.kusitms.presentation.model.profile.edit.categories
+import com.kusitms.presentation.ui.ImageVector.StudyIcon
 import com.kusitms.presentation.ui.ImageVector.icons.KusitmsIcons
 import com.kusitms.presentation.ui.ImageVector.icons.kusitmsicons.ArrowDown
+import com.kusitms.presentation.ui.login.member.TextColumn1
+import com.kusitms.presentation.ui.signIn.KusitmsInputField
+import com.kusitms.presentation.ui.signIn.SignInFixedInput
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -46,12 +53,27 @@ fun ProfileEditScreen(
     onBack: () -> Unit,
 ) {
     val expanded by viewModel.expended.collectAsStateWithLifecycle()
+    val email by viewModel.email.collectAsState()
+    val phoneNum by viewModel.phoneNum.collectAsState()
+    val name by viewModel.name.collectAsState()
+    val major by viewModel.major.collectAsState()
+    val selectedPart by viewModel.selectedPart.collectAsState()
+    val interests by viewModel.interests.collectAsState()
+    val likeCategoryText = if (interests.isNotEmpty()) {
+        interests.joinToString(", ") { it.content }
+    } else {
+        stringResource(id = R.string.signin_member_hint1_3)
+    }
+
+    var isOpenPartBottomSheet by remember { mutableStateOf(false) }
+    var isOpenLikeCategoryBottomSheet by remember { mutableStateOf(false) }
+
 
     Column {
         KusitsmTopBarBackTextWithIcon(
             text = stringResource(id = R.string.profile_edit_toolbar),
             onBackClick = {
-                          onBack()
+                onBack()
             },
         ) {
             Text(
@@ -88,11 +110,61 @@ fun ProfileEditScreen(
                 )
             }
         }
-        LazyColumn(
+        Column(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
+                .padding(20.dp)
                 .background(KusitmsColorPalette.current.Grey900)
+                .height(910.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
         ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(KusitmsColorPalette.current.Grey900)
+                    .height(64.dp),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Top
+
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start),
+                    verticalAlignment = Alignment.Top,
+                ) {
+                    StudyIcon.drawStudyIcon(
+                        modifier = Modifier
+                            .height(24.dp)
+                            .width(24.dp)
+                    )
+                    TextColumn1()
+                }
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(text = stringResource(id = R.string.signin_member_title1), style = KusitmsTypo.current.SubTitle2_Semibold, color = KusitmsColorPalette.current.Grey300)
+            Spacer(modifier = Modifier.height(28.dp))
+            Text(text = stringResource(id = R.string.signin_member_caption1_1), style = KusitmsTypo.current.Caption1, color = KusitmsColorPalette.current.Grey400)
+            Spacer(modifier = Modifier.height(5.dp))
+            SignInFixedInput(modelValue = name)
+            Spacer(modifier = Modifier.height(20.dp))
+
+            //전공
+            Text(text = stringResource(id = R.string.signin_member_caption1_2), style = KusitmsTypo.current.Caption1, color = KusitmsColorPalette.current.Grey400)
+            Spacer(modifier = Modifier.height(5.dp))
+            KusitmsInputField(
+                text = R.string.signin_member_hint1_1,
+                value = major,
+                onValueChange =  viewModel::updateMajor)
+            if(major.length > 20) {
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = "최대 20자까지 입력할 수 있어요",
+                    style = KusitmsTypo.current.Caption1,
+                    color = KusitmsColorPalette.current.Grey400
+                )
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
 
         }
     }
@@ -134,7 +206,6 @@ private fun ProfileFilterButton(
 }
 
 
-
 @Composable
 fun AllProfileFilterList(
     partNameList: List<ProfileFilterList>,
@@ -162,8 +233,6 @@ fun AllProfileFilterList(
         }
     }
 }
-
-
 
 
 @Preview(showBackground = true, showSystemUi = true)
